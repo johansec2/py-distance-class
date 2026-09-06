1f086c2358c55bca5eb159f567aaa6d1673c7291
Chip Johansen <chip.johansen99@gmail.com>
2026-09-05 20:27:26 -0400

    this is nuts, make up your mind

--- app/main.py
+++ app/main.py
@@ -31,12 +31,16 @@ class Distance:
         return self
 
     def __mul__(self, other: int | float) -> "Distance":
+        if isinstance(other, Distance):
+            raise TypeError("Cannot multiply Distance by Distance")
         return Distance(self.km * other)
 
     def __rmul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> "Distance":
+        if isinstance(other, Distance):
+            raise TypeError("Cannot divide Distance by Distance")
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Union["Distance", int, float]) -> bool:
